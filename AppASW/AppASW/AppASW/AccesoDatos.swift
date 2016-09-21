e17cab31c4c6033dce3caac682d4598e47b9cb6f
jdarjona <Administrador@mac-mini-de-administrador.local>
2016-09-21 10:01:13 +0200

    acceso a ws rest ventas

--- AppASW/AppASW/AppASW/AccesoDatos.swift
+++ AppASW/AppASW/AppASW/AccesoDatos.swift
@@ -13,8 +13,8 @@ import NetworkExtension
 import SystemConfiguration.CaptiveNetwork
 public class AccesoDatos: NSObject {
     
-   public var basicURL: String = "http://192.168.1.2/WSTRH/%@"
-   
+    public var basicURL: String = "http://192.168.1.2/WSTRH/%@"
+    public var bodyStr: String = ""
     
     /*func fetchSSIDInfo() ->  String {
         var currentSSID = ""
@@ -37,7 +37,85 @@ public class AccesoDatos: NSObject {
     
     */
     
+    public func getlogin(user:String, password:String,result:(result:Bool)->(Void)) {
     
+        //var flag: Bool = false
+        
+        if (user == "" || password == "") {
+            let alertView = UIAlertController(title: "Login failed",
+                                              message: "Wrong username or password." as String, preferredStyle:.Alert)
+            let okAction = UIAlertAction(title: "Try Again!", style: .Default, handler: nil)
+            alertView.addAction(okAction)
+            //self.presentViewController(alertView, animated: true, completion: nil)
+            //flag=false
+            result(result: false)
+            //return
+        }else{
+        
+        
+                
+                // Build the body message to request the token to the web app
+                self.bodyStr = "grant_type=password&username=" + user + "&password=" + password;
+                
+                // Setup the request
+                let myURL = NSURL(string: "http://intranet.trh-be.com/WSTRH/Token")!
+                let request = NSMutableURLRequest(URL: myURL)
+                request.HTTPMethod = "POST"
+                request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+                //request.setValue("application/json", forHTTPHeaderField: "Accept")
+                request.HTTPBody = bodyStr.dataUsingEncoding(NSUTF8StringEncoding)!
+                
+                let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+                    (data, response, error) -> Void in
+                    if let unwrappedData = data {
+                        print(unwrappedData)
+                        print("------")
+                        print(data)
+                        print("------")
+                        
+                        do {
+                            
+                            // Convert the Json object to an array of dictionaries
+                            let tokenDictionary:NSDictionary = try NSJSONSerialization.JSONObjectWithData(unwrappedData, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                            
+                            // Get the token
+                            let token:String = tokenDictionary["access_token"] as! String
+                            //flag=true
+                            print("------")
+                            print(token)
+                            print("------")
+                            // Keep record of the token
+                            //let saveToken:Bool = KeychainWrapper.setString(token, forKey: "access_token")
+                            //result(result: true)
+                            
+                            
+                            
+                            
+                        }
+                            
+                        catch {
+                            result(result: false)
+                            //flag=true
+                            
+//                            // Setup the alert
+//                            let alertView = UIAlertController(title: "Login failed",
+//                                                              message: "Wrong username or password." as String, preferredStyle:.Alert)
+//                            let okAction = UIAlertAction(title: "Try Again!", style: .Default, handler: nil)
+//                            alertView.addAction(okAction)
+//                            //self.presentViewController(alertView, animated: true, completion: nil)
+//                            return flag
+                        }
+                        
+                    }
+                    result(result: true)
+                }
+
+                task.resume()
+        }
+        //return
+        
+        
+    }
     
         private func isConnetedToNetWork() -> Bool {
         
@@ -156,6 +234,21 @@ public class AccesoDatos: NSObject {
             let pedido = PedidoVenta(json:json)
             
             result(result:pedido)
+        })
+        
+    }
+    
+    func getOferta(codOferta:String, result:(result:OfertaVenta)->Void)->Void{
+        
+        let codOfertaUrl = String(codOferta)
+        var url = "api/Ofertas?codOferta=%@"
+        url = String(format: url,codOfertaUrl)
+        //let pedidoVenta: PedidoVenta = PedidoVenta()
+        getJson(url ,DatosCallBack:{(json:String)->Void in
+            
+            let oferta = OfertaVenta(json:json)
+            
+            result(result:oferta)
         })
         
     }
