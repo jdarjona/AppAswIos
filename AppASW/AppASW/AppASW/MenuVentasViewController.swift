6e42c31b8ea5f5df63c9c82586265b065ec1eb09
jdarjona <Administrador@mac-mini-de-administrador.local>
2016-09-21 09:59:59 +0200

    menu de ventas

---
+++ AppASW/AppASW/AppASW/MenuVentasViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuVentasViewController.swift
+//  AppASW
+//
+//  Created by Administrador on 20/9/16.
+//  Copyright © 2016 Juande. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuVentasViewController: UIViewController {
+    
+    @IBOutlet weak var nuevaOferta: UIButton!
+    @IBOutlet weak var listadoOfertas: UIButton!
+    @IBOutlet weak var listadoPedidos: UIButton!
+    @IBOutlet weak var listadoProductos: UIButton!
+}
